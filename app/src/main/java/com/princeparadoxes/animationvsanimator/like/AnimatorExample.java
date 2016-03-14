d94d363012545b8602a7220c3af55f5811d061df
Danil Perevalov <tzarsuperman@yandex.ru>
2016-03-14 11:06:22 +0600

    Refacroring

--- app/src/main/java/com/princeparadoxes/animationvsanimator/like/AnimatorExample.java
+++ app/src/main/java/com/princeparadoxes/animationvsanimator/like/AnimatorExample.java
@@ -30,6 +30,7 @@ public class AnimatorExample {
             set.addListener(getLikeEndListener(view, icon));
             set.start();
         }
+        view.animate().alphaBy(0).alpha(1).start();
     }
 
     private static AnimatorListenerAdapter getLikeEndListener(final ImageView view, final int icon) {
@@ -57,9 +58,9 @@ public class AnimatorExample {
     private static AnimatorSet showAnimatorSet(View view) {
         AnimatorSet set = new AnimatorSet();
         set.setDuration(SHOW_DURATION).playTogether(
-                ObjectAnimator.ofFloat(view, "alpha", 0f, 1f),
-                ObjectAnimator.ofFloat(view, "scaleX", 0.2f, 1.4f),
-                ObjectAnimator.ofFloat(view, "scaleY", 0.2f, 1.4f)
+                ObjectAnimator.ofFloat(view, View.ALPHA, 0f, 1f),
+                ObjectAnimator.ofFloat(view, View.SCALE_X, 0.2f, 1.4f),
+                ObjectAnimator.ofFloat(view, View.SCALE_Y, 0.2f, 1.4f)
         );
         return set;
     }
@@ -67,8 +68,8 @@ public class AnimatorExample {
     private static AnimatorSet toNormalAnimatorSet(View view) {
         AnimatorSet set = new AnimatorSet();
         set.setDuration(TO_NORMAL_DURATION).playTogether(
-                ObjectAnimator.ofFloat(view, "scaleX", 1.4f, 1f),
-                ObjectAnimator.ofFloat(view, "scaleY", 1.4f, 1f)
+                ObjectAnimator.ofFloat(view, View.SCALE_X, 1.4f, 1f),
+                ObjectAnimator.ofFloat(view, View.SCALE_Y, 1.4f, 1f)
         );
         return set;
     }
@@ -76,9 +77,9 @@ public class AnimatorExample {
     private static AnimatorSet hideAnimatorSet(View view) {
         AnimatorSet set = new AnimatorSet();
         set.setDuration(HIDE_DURATION).playTogether(
-                ObjectAnimator.ofFloat(view, "alpha", 1f, 0f),
-                ObjectAnimator.ofFloat(view, "scaleX", 1f, 0.2f),
-                ObjectAnimator.ofFloat(view, "scaleY", 1f, 0.2f)
+                ObjectAnimator.ofFloat(view, View.ALPHA, 1f, 0f),
+                ObjectAnimator.ofFloat(view, View.SCALE_X, 1f, 0.2f),
+                ObjectAnimator.ofFloat(view, View.SCALE_Y, 1f, 0.2f)
         );
         set.setStartDelay(HIDE_DELAY);
         return set;
